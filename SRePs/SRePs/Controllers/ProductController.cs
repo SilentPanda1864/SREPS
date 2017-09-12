b99155c1bbf2fe06caa55f412300d3d240e9096e
RyannnnnnnR <ryan.reichenberg@gmail.com>
2017-09-12 18:01:29 +1000

    ADD: Build web api service connected to DB

---
+++ SRePs/SRePs/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SREPSDataAccess;
+
+namespace SRePs.Controllers
+{
+    public class ProductController : ApiController
+    {
+        //Get all products
+        public List<StockData> Get()
+        {
+            using(SREPSDBEntities entities = new SREPSDBEntities())
+            {
+                return entities.StockData.ToList();
+            }
+        }
+    }
+}
---
+++ SRePs/SRePs/Controllers/SalesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SREPSDataAccess;
+
+namespace SRePs.Controllers
+{
+    public class SalesController : ApiController
+    {
+        //Get all sales records
+        public List<SalesData> Get()
+        {
+            using (SREPSDBEntities enttites = new SREPSDBEntities())
+            {
+                return enttites.SalesData.ToList();
+            }
+        }
+    }
+}
