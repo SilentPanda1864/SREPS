61195862e759ad3f9cd657ab7ade86c036ded854
RyannnnnnnR <ryan.reichenberg@gmail.com>
2017-09-12 16:17:14 +1000

    ADD: Write database scripts for stock data and sales data

---
+++ SRePs/SRePs/Scripts/app/sql/SalesDataScript.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE SalesData
+(
+	[Sale_Id] INT NOT NULL,
+    [Product_Id] VARCHAR(15) NOT NULL, 
+    [Amount_Sold] INT NOT NULL, 
+    [Date_Sold] DATE NOT NULL, 
+    [Time_Sold] TIME NOT NULL, 
+	PRIMARY KEY(Sale_Id),
+    FOREIGN KEY (Product_Id) REFERENCES StockData(Product_Id)
+
+)
---
+++ SRePs/SRePs/Scripts/app/sql/StockDataScript.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE StockData
+(
+	Product_Id VARCHAR(15) NOT NULL PRIMARY KEY,
+	Product_Name VARCHAR(30) NOT NULL,
+	Stock_Level INT
+
+)
