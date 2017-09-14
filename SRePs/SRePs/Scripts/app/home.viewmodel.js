ebed8dcdc094e3f4b4fc3d43725e9b128ef599c4
RyannnnnnnR <ryan.reichenberg@gmail.com>
2017-09-14 12:13:39 +1000

    Add multiple products to record.

--- SRePs/SRePs/Scripts/app/home.viewmodel.js
+++ SRePs/SRePs/Scripts/app/home.viewmodel.js
@@ -1,4 +1,20 @@
-﻿function SalesModel() {
+﻿//Add additional text boxes
+$(document).ready(function () {
+	var max_fields = 10; //maximum input boxes allowed
+	var wrapper = $(".modal-body"); //Fields wrapper
+	var add_button = $("#addNewProduct"); //Add button ID
+
+	var x = 1; //initlal text box count
+	$(add_button).click(function (e) { //on add input button click
+		e.preventDefault();
+		if (x < max_fields) { //max input box allowed
+			x++; //text box increment
+			$(wrapper).append('<div class="row"><div class="col-md-6"><label class="form-control-label" for="inputProductName">Product Name</label><input id="inputProductName" class="form-control" type="text" name="productName" value="" /></div><div class="col-md-6 move-left"><label class="form-control-label" for="inputAmountSold">Amount Sold</label><input id="inputAmountSold" class="form-control" type="number" name="amountSold" value="0" /></div></div >'); //add input box
+		}
+	});
+});
+
+function SalesModel() {
 	var self = this;
 
 	self.salesRecords = ko.observableArray();
