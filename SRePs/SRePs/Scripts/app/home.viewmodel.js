5ec7e6bdf9bf86cd257fef66af414043059c3444
RyannnnnnnR <ryan.reichenberg@gmail.com>
2017-10-20 10:44:41 +1100

    UPDATE: Download csv for date range - https://halistechnology.com/2015/05/28/use-javascript-to-export-your-data-as-csv/

--- SRePs/SRePs/Scripts/app/home.viewmodel.js
+++ SRePs/SRePs/Scripts/app/home.viewmodel.js
@@ -90,8 +90,58 @@ function SalesModel() {
 		}
 		console.log("called");
 	}
+	self.convertArrayOfObjectsToCSV = function(args) {
+		var result, ctr, keys, columnDelimiter, lineDelimiter, data;
 
-self.startDate.subscribe(function(value) {
+		data = args.data || null;
+		if (data == null || !data.length) {
+			return null;
+		}
+
+		columnDelimiter = args.columnDelimiter || ',';
+		lineDelimiter = args.lineDelimiter || '\n';
+
+		keys = Object.keys(data[0]);
+
+		result = '';
+		result += keys.join(columnDelimiter);
+		result += lineDelimiter;
+
+		data.forEach(function (item) {
+			ctr = 0;
+			keys.forEach(function (key) {
+				if (ctr > 0) result += columnDelimiter;
+
+				result += item[key];
+				ctr++;
+			});
+			result += lineDelimiter;
+		});
+
+		return result;
+	}
+	self.downloadCSV = function(args) {
+		var data, filename, link;
+		var csv = self.convertArrayOfObjectsToCSV({
+			data: self.filteredSales()
+		});
+		if (csv == null) return;
+
+		filename = args.filename || 'export.csv';
+
+		if (!csv.match(/^data:text\/csv/i)) {
+			csv = 'data:text/csv;charset=utf-8,' + csv;
+		}
+		data = encodeURI(csv);
+
+		link = document.createElement('a');
+		link.setAttribute('href', data);
+		link.setAttribute('download', filename);
+		link.click();
+	}
+
+
+	self.startDate.subscribe(function(value) {
 			self.updateFilteredData();
 
 		});
